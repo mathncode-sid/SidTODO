fbd03fc992fca39c42febc1f55827de31fc524c5
BackendSid22 <sidneybarakamuriuki1@gmail.com>
2024-05-14 16:54:14 +0300

    I cannot believe I haven't been committing!

---
+++ TODOAPP.py
@@ -0,0 +1,36 @@
+
+while True:
+    user_action = input("Type add, show, edit, complete or exit: ")
+    user_action = user_action.strip()
+
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ") + "\n"
+
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
+            todos.append(todo)
+
+            file = open('todos.txt', 'w')
+            file.writelines(todos)
+            file.close()
+        case 'show' | 'display':
+            for index, item in enumerate (todos):
+                item = item.title()
+                row = f"{index + 1}-{item}"
+                print(row)
+        case 'edit':
+            print("Got it!")
+            number = int(input("What is the number of the todo you want to edit?"))
+            number = number - 1
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo
+        case 'complete':
+            number = int(input("What number of the todo do you want to mark as complete?"))
+            todos.pop(number - 1)
+        case 'exit':
+            break
+
+print("Adios")
