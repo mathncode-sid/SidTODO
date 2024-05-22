ad4299e0c86e4e7cc1a74993ffd13c136ca2b53b
BackendSid22 <168301526+BackendSid22@users.noreply.github.com>
2024-05-22 21:56:46 +0300

    Optimised code and included input into the custom function.

--- TODOAPP.py
+++ TODOAPP.py
@@ -1,5 +1,5 @@
-def get_todos():
-    with open('todos.txt', 'r') as file_local:
+def get_todos(filepath):
+    with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
@@ -11,7 +11,7 @@ while True:
     if user_action.startswith("add"):
         todo = user_action[4:].strip()
 
-        todos = get_todos()
+        todos = get_todos("todos.txt")
 
         todos.append(todo + '\n')
 
@@ -20,7 +20,7 @@ while True:
 
     elif user_action.startswith("show"):
 
-        todos = get_todos()
+        todos = get_todos("todos.txt")
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -34,7 +34,7 @@ while True:
             print(number)
             number = number - 1
 
-            todos = get_todos()
+            todos = get_todos("todos.txt")
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + '\n'
@@ -54,7 +54,7 @@ while True:
                 print("Please specify a number to complete.")
                 continue
 
-            todos = get_todos()
+            todos = get_todos("todos.txt")
 
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
