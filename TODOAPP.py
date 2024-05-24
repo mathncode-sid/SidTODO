3eb36561ff8e48a71b3e8023b469c82d511996e4
BackendSid22 <168301526+BackendSid22@users.noreply.github.com>
2024-05-25 00:09:09 +0300

    Multiple arguments to optimize code

--- TODOAPP.py
+++ TODOAPP.py
@@ -4,6 +4,11 @@ def get_todos(filepath):
     return todos_local
 
 
+def write_todos(filepath, todos_arg):
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
@@ -15,8 +20,7 @@ while True:
 
         todos.append(todo + '\n')
 
-        with open('todos.txt', 'w') as file:
-            file.writelines(todos)
+        write_todos("todos.txt", todos)
 
     elif user_action.startswith("show"):
 
@@ -39,8 +43,7 @@ while True:
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + '\n'
 
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+            write_todos("todos.txt", todos_arg=todos)
 
         except ValueError:
             print("Your command is not valid.")
@@ -60,8 +63,7 @@ while True:
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+            write_todos("todos.txt", todos_arg=todos)
 
             message = f"Todo {todo_to_remove} was removed from the list."
             print(message)
