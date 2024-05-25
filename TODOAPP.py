e73e7e230681a672eba8a2f7ffc77da0d591c676
BackendSid22 <168301526+BackendSid22@users.noreply.github.com>
2024-05-25 18:21:16 +0300

    moved custom functions to a new file and used import to get functions from the file

--- TODOAPP.py
+++ TODOAPP.py
@@ -1,17 +1,6 @@
-def get_todos(filepath="todos.txt"):
-    """ Read a text file and return the list of
-    to-do items.
-    """
-    with open(filepath, 'r') as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
+# from functions import get_todos, write_todos
 
-
-def write_todos(todos_arg, filepath="todos.txt"):
-    """ Write a to-do items list in a text file."""
-    with open(filepath, 'w') as file:
-        file.writelines(todos_arg)
-
+import functions
 
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
@@ -20,15 +9,15 @@ while True:
     if user_action.startswith("add"):
         todo = user_action[4:].strip()
 
-        todos = get_todos()
+        todos = functions.get_todos()
 
         todos.append(todo + '\n')
 
-        write_todos(todos_arg=todos)
+        functions.write_todos(todos_arg=todos)
 
     elif user_action.startswith("show"):
 
-        todos = get_todos()
+        todos = functions.get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -42,12 +31,12 @@ while True:
             print(number)
             number = number - 1
 
-            todos = get_todos()
+            todos = functions.get_todos()
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + '\n'
 
-            write_todos(todos_arg=todos)
+            functions.write_todos(todos_arg=todos)
 
         except ValueError:
             print("Your command is not valid.")
@@ -61,13 +50,13 @@ while True:
                 print("Please specify a number to complete.")
                 continue
 
-            todos = get_todos()
+            todos = functions.get_todos()
 
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
-            write_todos(todos_arg=todos)
+            functions.write_todos(todos_arg=todos)
 
             message = f"Todo {todo_to_remove} was removed from the list."
             print(message)
