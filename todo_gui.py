0be7072bcb35d3f313bd1c72137f3140937e101f
BackendSid22 <168301526+BackendSid22@users.noreply.github.com>
2024-05-31 20:03:48 +0300

    Added the Complete and Exit buttons

--- todo_gui.py
+++ todo_gui.py
@@ -7,9 +7,14 @@ add_button = sg.Button("Add")
 edit_button = sg.Button("Edit")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=[45, 10])
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
+
+layout = [[label], [input_box, add_button], [list_box, edit_button, complete_button],
+          [exit_button]]
 
 window = sg.Window('To-Do App',
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=layout,
                    font=('Helvetica', 20))
 while True:
     event, values = window.read()
@@ -35,7 +40,15 @@ while True:
             todos[index] = new_todo + "\n"
             functions.write_todos(todos)
             window['todos'].update(values=[todo.strip() for todo in todos])
-
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+        case "Exit":
+            break
         case 'todos':
             window['todo'].update(value=values['todos'][0])
 
