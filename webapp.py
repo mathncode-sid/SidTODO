f25a50df3ad173e647b3f09eb41849477d654d10
BackendSid22 <168301526+BackendSid22@users.noreply.github.com>
2024-06-02 18:55:57 +0300

    Implemented a new feature "Add new todo"

--- webapp.py
+++ webapp.py
@@ -2,12 +2,18 @@ import streamlit as st
 
 import functions
 
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
 todos = functions.get_todos()
 
 st.title("TODO APP")
 st.subheader("This is my TODO App")
 st.write("This is a simple app to enhance your productivity")
-
 
 st.checkbox("Buy Grocery")
 st.checkbox("Throw the trash")
@@ -15,4 +21,5 @@ st.checkbox("Throw the trash")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Add new todo")
+st.text_input(label="", placeholder="Add new todo",
+              on_change=add_todo, key='new_todo')
