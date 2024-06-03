084b12ff1cfe1e864c8e58d0b67390004dbbb9e5
BackendSid22 <168301526+BackendSid22@users.noreply.github.com>
2024-06-03 14:18:46 +0300

    Implemented the complete button and its functionality.

--- functions.py
+++ functions.py
@@ -7,7 +7,23 @@ def get_todos(filepath="todos.txt"):
     return todos_local
 
 
+def get_completed_todos(filepath="completed_todos.txt"):
+    """ Read a text file and return the list of
+    completed to-do items.
+    """
+    with open(filepath, 'r') as file_local:
+        completed_todos_local = file_local.readlines()
+    return completed_todos_local
+
+
 def write_todos(todos_arg, filepath="todos.txt"):
     """ Write a to-do items list in a text file."""
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
+
+
+def write_completed_todos(completed_todos_arg, filepath="completed_todos.txt"):
+    """ Write a completed to-do items list in a text file."""
+    with open(filepath, 'w') as file:
+        file.writelines(completed_todos_arg)
+
--- webapp.py
+++ webapp.py
@@ -1,25 +1,39 @@
 import streamlit as st
+import os
 
 import functions
 
-
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
     functions.write_todos(todos)
+    st.session_state["new_todo"] = ""
+
+
+def mark_complete(event, todo):
+    completed_todos.append(todo)
+    todos.remove(todo)
+    functions.write_todos(todos)
+    del st.session_state[todo]
 
 
 todos = functions.get_todos()
+completed_todos = functions.get_completed_todos()
 
 st.title("TODO APP")
 st.subheader("This is my TODO App")
 st.write("This is a simple app to enhance your productivity")
 
-st.checkbox("Buy Grocery")
-st.checkbox("Throw the trash")
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        st.session_state["todo_to_complete"] = todo
+        if st.button("Complete", on_click=mark_complete, args=(todo, todo)):
+            st.session_state["todo_to_complete"] = todo
 
-for todo in todos:
-    st.checkbox(todo)
-
-st.text_input(label="", placeholder="Add new todo",
-              on_change=add_todo, key='new_todo')
+for completed_todo in completed_todos:
+    st.write(completed_todo)
+st.text_input(label="TODO", placeholder="Add new todo",
+              on_change=add_todo, key='new_todo', label_visibility="hidden")
+st.button("Add", on_click=add_todo)
